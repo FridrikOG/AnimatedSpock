0e9ef965e44155c48f67ccc04bf0a2ee315b566a
Torarinn gunnarsson <torarinngunnarsson@Torarinns-MacBook-Pro.local>
2018-12-04 18:09:32 +0000

    csv writer implemented

--- repositories/CustomerRepository.py
+++ repositories/CustomerRepository.py
@@ -1,12 +1,14 @@
 from models.Customer import Customer
+import csv
 
 class CustomerRepository:
     def __init__(self):
         self.__customers = []
 
     def addCustomer(self,customer):
-        # with open('./data/customers.txt','a') as customerFile:
-        #     name = customer.getName()
-        #     age = customer.getAge()
-        #     ssn = customer.getSsn()
-        #     customerFile.write(f'{name},{age},{ssn}\n')
+        with open('./data/customers.csv','a',) as customerFile:
+            writer = csv.writer(customerFile)
+            name = customer.getName()
+            age = customer.getAge()
+            ssn = customer.getSsn()
+            customerFile.write(f'{name},{age},{ssn}\n')
